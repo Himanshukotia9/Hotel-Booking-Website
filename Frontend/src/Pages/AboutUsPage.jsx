edaa1d0b45c9d49395a4296ee7e8a3da1edefff7
Himanshu kotia <himanshukotia9@gmail.com>
2024-11-02 14:56:48 +0530

    test6

--- Frontend/src/Pages/AboutUsPage.jsx
+++ Frontend/src/Pages/AboutUsPage.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import PageBanner from '../Components/PageBanner'
+import { Typography } from "@material-tailwind/react";
+import Rooms from '../Components/Rooms';
+import PageBanner from '../Components/PageBanner';
+import ListComp from '../Components/ListComp';
+import { cardsData, roomPolicy } from '../data';
 
 export default function AboutUsPage() {
   return (
