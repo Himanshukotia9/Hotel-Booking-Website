345ac6d989932b20275b8987c0616a8f5292b2a5
Himanshu kotia <himanshukotia9@gmail.com>
2024-11-02 14:52:28 +0530

    test4

--- Frontend/src/Pages/RoomsPage.jsx
+++ Frontend/src/Pages/RoomsPage.jsx
@@ -10,11 +10,30 @@ export default function RoomsPage() {
     <div className="">
         <PageBanner 
             img={'bg-aboutBanner'}
-            height={'h-[80vh]'}
+            height={'h-[600px]'}
             title={"Welcome to The Pink Magnolia Cottage"}
             text={"Experience serene luxury in the heart of Mussoorie's hills."}
         />
-        
+        <div className='py-12 mx-4'>
+            {/* <h1 className="text-3xl md:text-5xl font-bold font-serif text-blue-gray-800 text-center mb-12">Check for Room Availability</h1> */}
+            <Typography
+            variant="h6"
+                className="font-bold opacity-80 text-3xl md:text-5xl text-center font-serif mb-12"
+                >
+                Check for Room Availability
+                </Typography>
+            {cardsData.map((card, index) => (
+                <Rooms
+                key={card.id}
+                title={card.title}
+                subTitle={card.subTitle}
+                description={card.description}
+                imageUrl={card.imageUrl}
+                reverse={index % 2 !== 1} // Alternate image position
+                />
+            ))}
+            <ListComp points={roomPolicy} titlestyle={"font-bold text-2xl md:text-4xl"} pointsstyle={"font-bold text-xl md:text-2xl"}/>
+        </div>
     </div>
   )
 }
